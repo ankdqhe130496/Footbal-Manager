c54cb9f7e402fee2fd6d2379050a935a46b62554
Vu Hai Lam <lamvh2812@gmail.com>
2021-12-15 01:02:16 +0700

    Init

---
+++ Entities/Accout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FootbalManager.Entities
+{
+    public class Accout
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+
+        public Accout(string account, string password)
+        {
+            Account = account;
+            Password = password;
+        }
+
+    }
+}
